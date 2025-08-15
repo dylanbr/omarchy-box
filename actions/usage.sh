da8438e5cdb9797de74d2d97c759f99fcc4f0b12
Dylan Bridgman <light@ether.org.za>
2025-08-15 18:04:51 +0200

    Update usage

--- actions/usage.sh
+++ actions/usage.sh
@@ -3,18 +3,15 @@
 cat <<EOF
 Usage: $0 <command> [options]
 
-Commands:
-  clean      Clear all data
+Primary Commands:
+  build      Build the Omarchy image by running the installer
+  start      Start the last built Omarchy image
+
+Other Commands:
   download   Download and verify the base Arch Linux image
+  clean      Erase all data and start fresh
   help       Show this message
 
-Options:
-  -n, --name NAME     Name or identifier
-  -h, --help          Show this help message
-
-Examples:
-  $0 clean
-  $0 download
 EOF
 
 exit 1
